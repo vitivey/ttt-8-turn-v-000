c15689846b6062169a469fc8f4cbe4e65087f562
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-02 02:56:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,11 +43,14 @@ def position_taken?(board, index)
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(board, index)
+      until valid_move?(board, index) == true
+        puts "Please enter 1-9:"
+        input = gets.strip
+        index = input_to_index(input)
+        valid_move?(board, index)
+      end
+    else
       move(board, index, character = "X")
-      else
-        until valid_move?(board, index)
-        turn(board)
-        end
     end
     puts display_board(board)
   end
